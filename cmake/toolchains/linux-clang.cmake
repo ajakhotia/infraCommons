02a737147c0e579acf52e6598480d2c480892036
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2026-08-09 09:41:45 +0200

    feat(cmake): add Linux clang and GNU toolchain files with CUDA support

---
+++ cmake/toolchains/linux-clang.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+set(CMAKE_Fortran_COMPILER /usr/bin/flang)
+
+find_program(NVCC_EXECUTABLE nvcc HINTS ENV CUDA_HOME ENV CUDA_PATH /usr/local/cuda PATH_SUFFIXES bin)
+
+if(NVCC_EXECUTABLE)
+  set(CMAKE_CUDA_COMPILER "${NVCC_EXECUTABLE}")
+  set(CMAKE_CUDA_ARCHITECTURES 75;80)
+  set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
+  set(CMAKE_CUDA_FLAGS_INIT "${CMAKE_CUDA_FLAGS_INIT} -allow-unsupported-compiler")
+endif()
